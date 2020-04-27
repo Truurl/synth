000a0618f2f95fdabfa0ab41eccdaf46bbb549d2
Truuul <albertjakiela@gmail.com>
2020-04-27 09:59:05 +0200

    added same basic wave generation

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -47,5 +47,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/ProjektAMK.elf.dir/DependInfo.cmake"
+  "CMakeFiles/Synth.elf.dir/DependInfo.cmake"
   )
